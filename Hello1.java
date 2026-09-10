55b3b66860347d46d26d226dd3011b0f0cde604f
Satyam Kumar Thakur <satyamthakur1821@gmail.com>
2026-09-10 21:34:41 +0530

    first commit

---
+++ Hello1.java
@@ -0,0 +1,20 @@
+public class Hello1 {
+
+    public static void main(String[] args) {
+        // variables
+        String name = "vivek";
+        System.out.println(name);
+
+        int myNum = 15;
+        System.out.println(myNum);
+
+        int mySum;
+        mySum = 18;
+        System.out.println(mySum);
+
+        int myVum = 20;
+        myVum = 25;
+        System.out.println(myVum);
+
+    }
+}
