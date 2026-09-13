caac4de591bccdc75db01bfe388560de266ae364
Satyam Kumar Thakur <satyamthakur1821@gmail.com>
2026-09-13 16:24:24 +0530

    MultipleVariables

---
+++ MultipleVariables.java
@@ -0,0 +1,17 @@
+public class MultipleVariables {
+
+    public static void main(String[] args) {
+        int x = 5;
+        int y = 10;
+        int z = 15;
+        System.out.println(x + y + z); // 30
+
+        int a = 5, b = 10, c = 20;
+        System.out.println(a + b + c); // 35
+
+        int u, v, w;
+        u = v = w = 50;
+        System.out.println(u + v + w);
+
+    }
+}
