60d94449f1c4d45b037049b6479236393a84d0c0
Satyam Kumar Thakur <satyamthakur1821@gmail.com>
2026-09-15 19:30:47 +0530

    data types

---
+++ DataTypes.java
@@ -0,0 +1,15 @@
+public class DataTypes {
+    public static void main(String[] args) {
+        int myNum = 15;
+        float myFloatNum = 35f;
+        char myChar = 'd';
+        boolean myBool = false;
+        String myText = "vivek";
+
+        int myGum = 55;
+        // myGum="hello"; // cannot assign a string to an int
+
+        String myBext = "bye";
+        // myBext=255; // cannot assign a number to a String
+    }
+}
