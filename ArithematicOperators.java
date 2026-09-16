7beee95b6ff2c43e3f6278f5ead65ab83f34d37b
Satyam Kumar Thakur <satyamthakur1821@gmail.com>
2026-09-16 23:02:34 +0530

    ArithematicOperators

---
+++ ArithematicOperators.java
@@ -0,0 +1,55 @@
+public class ArithematicOperators {
+    public static void main(String[] args) {
+
+       // Arithematic Operators
+
+        int sum1 = 100 + 50;
+        int sum2 = sum1 + 200;
+        int sum3 = sum2 + sum2;
+
+        System.out.println(sum1); // 150
+        System.out.println(sum2); // 350
+        System.out.println(sum3); // 700
+
+        int x = 10;
+        int y = 5;
+
+        System.out.println(x + y); // 15
+        System.out.println(x - y); // 5
+        System.out.println(x * y); // 50
+        System.out.println(x / y); // 2
+        System.out.println(x % y); // 0
+
+        int z = 5;
+        ++z;
+        System.out.println(z); // 6
+        --z;
+        System.out.println(z); // 5 kyuki z ki value badal chuki thi 5 se 6
+
+        int c = 10;
+        int d = 3;
+        System.out.println(c / d); // 3
+
+        double m = 10.0d;
+        double n = 3.0d;
+        System.out.println(m / n); // 3.33333333335
+
+        int peopleInRoom = 0;
+
+        peopleInRoom++;
+        peopleInRoom++;
+        peopleInRoom++;
+
+        System.out.println(peopleInRoom);  // 3 
+
+        peopleInRoom--;
+
+        System.out.println(peopleInRoom);  // 2
+
+
+        
+
+        
+
+    }
+}
