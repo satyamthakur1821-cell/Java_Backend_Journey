1f824673c62a958a9ba7300e88741fddac2c5372
Satyam Kumar Thakur <satyamthakur1821@gmail.com>
2026-09-17 21:58:57 +0530

    ComparisonOperators

---
+++ ComparisonOperators.java
@@ -0,0 +1,23 @@
+public class ComparisonOperators {
+    public static void main(String[] args) {
+
+        // Comparison Operators
+
+        int x = 5;
+        int y = 10;
+        System.out.println(x == y); // true
+
+        int a = 6;
+        int b = 3;
+        System.out.println(a != b); // ! means not equalt to that why it is tyue
+
+        int c = 9;
+        int d = 3;
+        System.out.println(c > d); // true
+
+        int e = 5;
+        int f = 10;
+        System.out.println(f >= e); // true
+
+    }
+}
