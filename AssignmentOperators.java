52ebe554284e3e26695f058d6e5b1ea61090cd84
Satyam Kumar Thakur <satyamthakur1821@gmail.com>
2026-09-17 21:26:53 +0530

    AssignmentOperators

---
+++ AssignmentOperators.java
@@ -0,0 +1,23 @@
+public class AssignmentOperators {
+
+    public static void main(String[] args) {
+
+       // Assignment Operators
+
+        int x = 5;
+        x += 4;
+        System.out.println(x); // 9
+
+        int y = 10;
+        y -= 5;
+        System.out.println(y); // 5
+
+        int z = 10;
+        z *= 5;
+        System.out.println(z); // 50
+
+        int savings = 5000;
+        savings += 500;
+        System.out.println(savings);  // 5500
+    }
+}
